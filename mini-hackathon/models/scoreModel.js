9f84a77c29d713c8509e98e5ee297acaa44847cf
Tommy Le <minhtu.le128@gmail.com>
2019-01-21 00:58:57 +0700

    21/1/2019

---
+++ mini-hackathon/models/scoreModel.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ScoreSchema = new Schema ({
+    
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model("Score", ScoreSchema);
